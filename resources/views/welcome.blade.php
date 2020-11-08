fce62a121bbb63d822926d9987684197b9ff145c
chrisva_co <chvc2003@hotmail.com>
2020-11-08 15:24:38 -0500

    feat: se añade configuración de directorios cache

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PruebaUP!
                 </div>
 
                 <div class="links">
